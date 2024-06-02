d3c1a5dbbb816bf0368313a818c8e3a4364e0e73
yk-sora <165569035+yk-sora@users.noreply.github.com>
2024-06-02 04:39:05 +0000

    Finish 応用課題3

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_061455) do
+ActiveRecord::Schema.define(version: 2024_06_02_024410) do
 
   create_table "books", force: :cascade do |t|
-    t.text "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "title"
   end
 
 end
